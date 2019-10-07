bcebcd6e7e8de250b89e3e7202a4258866d8e8d8
Ahsverus <arthursand1411@gmail.com>
2019-10-07 16:28:32 -0300

    add axios and info storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import logo from './assets/logo.svg'
-
+import api from './services/api'
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const response = await api.post('/user', { email })
+    const { _id } = response.data
+    localStorage.setItem('user', _id)
+
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Aircnc" />
 
       <div className="content">
         <p>
-          Ofereça <strong>spots</strong> para programadores e econtre <stron>talentos</stron> para sua empresa
+          Ofereça <strong>spots</strong> para programadores e econtre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email *</label>
           <input
             type="email"
             id="email"
-            placeholder="Seu melhor email" />
+            value={email}
+            placeholder="Seu melhor email"
+            onChange={event => setEmail(event.target.value)}
+          />
           <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
